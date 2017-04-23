482eb1072c7159619bc563d0e2e4dc87f5f04c72
carloslimasis <carloslimasis@gmail.com>
2017-04-23 00:31:28 -0300

    First commit

---
+++ src/main/java/com/algaworks/brewer/config/init/AppInitializer.java
@@ -0,0 +1,34 @@
+package com.algaworks.brewer.config.init;
+
+import javax.servlet.Filter;
+
+import org.springframework.web.filter.CharacterEncodingFilter;
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+import com.algaworks.brewer.config.WebConfig;
+
+public class AppInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+
+	@Override
+	protected Class<?>[] getRootConfigClasses() {
+		return null;
+	}
+
+	@Override
+	protected Class<?>[] getServletConfigClasses() {
+		return new Class<?>[] { WebConfig.class };
+	}
+
+	@Override
+	protected String[] getServletMappings() {
+		return new String[] { "/" };
+	}
+
+	@Override
+	protected Filter[] getServletFilters() {
+		CharacterEncodingFilter characterEncondingFilter = new CharacterEncodingFilter();
+		characterEncondingFilter.setEncoding("UTF-8");
+		characterEncondingFilter.setForceEncoding(true);
+		return new Filter[] { characterEncondingFilter };
+	}
+}
